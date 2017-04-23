11a1f381dd16506952fac39f3ac0ec119cd340f0
raphael ferreira <raphaelbsferreira@gmail.com>
2017-04-23 02:38:12 -0300

    delete event on index html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
  * Created by raphael on 19/04/17.
  */
-import {getUsers} from './api/userApi'
+import {getUsers, deleteUser} from './api/userApi'
 
 getUsers().then(result => {
   let usersBody = result.reduce((res, user) => res += `<tr>
@@ -13,4 +13,16 @@ getUsers().then(result => {
     </tr>` , "")
 
   global.document.getElementById('users').innerHTML = usersBody
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser')
+
+  Array.from(deleteLinks, link => {
+    link.onclick = (event) => {
+      const element = event.target
+      event.preventDefault()
+      deleteUser(element.getAttribute('data-id'))
+      const row = element.parentNode.parentNode
+      row.parentNode.removeChild(row)
+    }
+  })
 })
