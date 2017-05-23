4cb7d1ae6f98bf45b06ee18a9563e08197793370
Raphael Ferreira <raphaelbsferreira@gmail.com>
2017-05-22 23:19:41 -0300

    linting fixes

--- spec/testSetup.js
+++ spec/testSetup.js
@@ -1,8 +1,8 @@
 /**
  * Created by raphael on 20/04/17.
  */
+/* eslint-disable func-names */
 
-require('babel-register')()
+require('babel-register')();
 
-require.extensions['.css'] = function () {
-}
+require.extensions['.css'] = function () { };
